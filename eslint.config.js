d8b731addb031d7950ee28cfcd290de584076648
weiyafei <2553241022@qq.com>
2023-12-11 20:38:14 +0800

    feat(init): 初始化

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-12-11 19:54:02
+ * @LastEditTime: 2023-12-11 20:27:15
+ * @LastEditors: wsy
+ */
+import antfu from '@antfu/eslint-config'
+
+export default antfu()
