9bc46957b92df88593533f85003bcc1f6526ef21
weiyafei <2553241022@qq.com>
2023-12-12 19:37:04 +0800

    feat(playground): alias 测试环境

--- playground/alias/rollup.config.ts
+++ playground/alias/rollup.config.ts
@@ -1,3 +1,17 @@
-export default {
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-12-11 21:08:12
+ * @LastEditTime: 2023-12-12 19:12:59
+ * @LastEditors: wsy
+ */
+import { defineConfig } from 'rollup'
 
-}
+export default defineConfig({
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins:[]
+})
--- playground/alias/src/index.ts
+++ playground/alias/src/index.ts
@@ -1,2 +1,10 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-12-11 21:07:27
+ * @LastEditTime: 2023-12-12 19:31:17
+ * @LastEditors: wsy
+ */
 const test = '123'
-export default test
+console.log(test)
+export {}
